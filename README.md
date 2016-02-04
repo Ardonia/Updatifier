53b3fc3c3907df09ef058f568339a0dc6c604ac3
Flibio <flibio@users.noreply.github.com>
2016-02-04 12:37:55 -0700

    Initial Updatifier commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Updatifier
-Checks for updates to Sponge plugins
+
+Updatifier is a simple Sponge plugin that allows other plugins to easily check if they have an update available.
+
+###Server Administrators
+
+To install Updatifier on your Sponge server, simply move the JAR into your `mods` folder. Plugin developers need to add support for Updatifier to their plugins in order for Updatifier to check for updates to that plugin. Updatifier is not required, meaning if you download a plugin that has support for Updatifier, and you do not have Updatifier installed, no errors will be thrown to the console.
+
+###Plugin Developers
+
+It is extremely simple to add Updatifier support into your plugin. There are two simple steps to follow:
+
+1. Add Updatifier as a Maven or Gradle dependency.
+2. Annotate your main plugin class and add Updatifier as an `after` dependency.
+
+#####Adding Updatifier as a Maven/Gradle Dependency
+
+Updatifier uses [**JitPack**](https://jitpack.io/) to allow you to add Updatifier as a Maven/Gradle dependency. To get started you need to add JitPack as a repository in your pom.xml:
+
+```xml
+<repository>
+    <id>jitpack.io</id>
+    <url>https://jitpack.io</url>
+</repository>
+```
+
+Next you will want to add Updatifer as a dependency:
+
+```xml
+<dependency>
+    <groupId>com.github.Flibio</groupId>
+    <artifactId>EconomyLite</artifactId>
+    <version>RELEASE</version>
+</dependency>
+```
+
+The steps are the essentially the same for Gradle, with just a few formatting changes.
+
+#####Annotating Your Main Plugin Class
+
+In order for Updatifier to check for updates to your plugin, it needs to know some information about your plugin. To do that, you must annotate your main plugin class with `@Updatifier`, similar to how you annotate for Sponge with `@Plugin`. You also must add Updatifier as an `after` dependency. An example can be seen below.
+
+```java
+@Updatifier(repoName = "EconomyLite", repoOwner = "Flibio", version = "v1.1.0")
+@Plugin(id = "EconomyLite", name = "EconomyLite", version = "1.1.0", dependencies = "after: Updatifier")
+public class MyClass {
+
+}
+```
+
+Updatifier is not required, meaning if you add support for Updatifier, and a server owner does not have Updatifier installed, no errors will be thrown to the console. The Updatifier annotation will simply be ignored.
---
+++ src/me/flibio/updatifier/UpdatifierPlugin.java
@@ -0,0 +1,75 @@
+package me.flibio.updatifier;
+
+import org.slf4j.Logger;
+import org.spongepowered.api.Sponge;
+import org.spongepowered.api.entity.living.player.Player;
+import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameInitializationEvent;
+import org.spongepowered.api.event.game.state.GameStartedServerEvent;
+import org.spongepowered.api.event.network.ClientConnectionEvent;
+import org.spongepowered.api.plugin.Plugin;
+import org.spongepowered.api.plugin.PluginContainer;
+import org.spongepowered.api.scheduler.Scheduler;
+import org.spongepowered.api.text.Text;
+import org.spongepowered.api.text.format.TextColors;
+
+import com.google.inject.Inject;
+
+import java.lang.annotation.Annotation;
+import java.util.HashMap;
+
+@Plugin(id = "Updatifier", name = "Updatifier", version = "1.0.0")
+public class UpdatifierPlugin {
+	
+	private HashMap<String, String> updates = new HashMap<String, String>();
+	
+	@Inject
+	Logger logger;
+	
+	private UpdatifierAPI api = new UpdatifierAPI();
+	
+	private Scheduler scheduler;
+	
+	@Listener
+	public void onInitialize(GameInitializationEvent event) {
+		//Register the Updatifier API
+		Sponge.getGame().getServiceManager().setProvider(this, UpdatifierAPI.class, api);
+		//Set the scheduler
+		this.scheduler = Sponge.getScheduler();
+	}
+	
+	@Listener
+	public void started(GameStartedServerEvent event) {
+		for(PluginContainer pluginC : Sponge.getPluginManager().getPlugins()) {
+			if(pluginC.getInstance().isPresent()) {
+				if(pluginC.getInstance().get().getClass().isAnnotationPresent(Updatifier.class)) {
+					Annotation annotation = pluginC.getInstance().get().getClass().getAnnotation(Updatifier.class);
+					Updatifier info = (Updatifier) annotation;
+					scheduler.createTaskBuilder().execute(r -> {
+						boolean available = api.updateAvailable(info.repoOwner(), info.repoName(), info.version());
+						if(available) {
+							//Add the plugin to the HashMap
+							updates.put(pluginC.getName(), info.repoOwner()+"/"+info.repoName());
+							//Log the messages on the main thread (Makes the messages shorter in length)
+							scheduler.createTaskBuilder().execute(c -> {
+								logger.info("An update is available for "+pluginC.getName()+"!");
+								logger.info("Download it here: "+"https://github.com/"+info.repoOwner()+"/"+info.repoName()+"/releases");
+							}).submit(this);
+						}
+					}).async().submit(this);
+				}
+			}
+		}
+	}
+	
+	@Listener
+	public void onJoin(ClientConnectionEvent.Join event) {
+		Player player = event.getTargetEntity();
+		if(player.hasPermission("updatifier.notify")) {
+			for(String name : updates.keySet()) {
+				player.sendMessage(Text.of(TextColors.YELLOW,"An update is available for ",TextColors.GREEN,name,"!"));
+				player.sendMessage(Text.of(TextColors.GRAY,"https://github.com/"+updates.get(name)+"/releases"));
+			}
+		}
+	}
+}
